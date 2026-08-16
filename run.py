1bd086c22cbeb162eaa73fa1683a8f7465ddf662
Shakib Belal <shakib.belal17@gmail.com>
2026-08-16 21:28:50 +0100

    feat: pipeline orchestration run.py

--- run.py
+++ run.py
@@ -21,16 +21,17 @@ def main():
     source_df = fetch_stock_data().as_pandas()
     source_df = source_df.reset_index()
     is_valid = validate_data(source_df)
+
+    # Transform
     transformed_df = transform_stock_data(source_df)
+
+    # Load
     store_data(transformed_df)
     enforce_retention()
 
+    # Calculate stats
     latest_close = get_latest_close()
     daily_change = get_daily_change()
-
-    print(latest_close)
-    print(daily_change)
-
 
 
 
